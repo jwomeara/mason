b054f4d85c7274f07ebc43ec3fc4db2e455cc79a
Whitney O'Meara <womeara@radiantblue.com>
2015-05-18 10:42:00 -0400

    Initial version of geowave-jace

---
+++ script.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+MASON_NAME=geowave-jace
+MASON_VERSION=0.8.9
+MASON_LIB_FILE=lib/libjace.a
+
+. ${MASON_DIR:-~/.mason}/mason.sh
+
+function mason_load_source {
+    mason_download http://s3.amazonaws.com/geowave-rpms/release/TARBALL/geowave-0.8.9-9639eb4-jace-source.tar.gz \
+    6c6def77cc02ede781876ef095a3583e501c5acd
+
+    mason_extract_tar_gz
+    
+    export MASON_BUILD_PATH=${MASON_ROOT}/.build
+}
+
+function dep() {
+    ${MASON_DIR:-~/.mason}/mason install $1 $2
+    ${MASON_DIR:-~/.mason}/mason link $1 $2
+}
+
+function all_deps() {
+    dep cmake 3.2.2 &
+    dep boost 1.57.0 &
+    dep boost_libsystem 1.57.0 &
+    dep boost_libthread 1.57.0 &
+    wait
+}
+
+function mason_prepare_compile {
+    cd $(dirname ${MASON_ROOT})
+    all_deps
+    MASON_HOME=${MASON_ROOT}/.link
+    PATH=${MASON_HOME}/bin:$PATH
+}
+
+function mason_compile {
+    mkdir -p build
+    cd build
+    cmake ../ \
+      -DCMAKE_INSTALL_PREFIX=${MASON_PREFIX} \
+      -DBOOST_INCLUDEDIR=${MASON_HOME}/include \
+      -DBOOST_LIBRARYDIR=${MASON_HOME}/lib \
+      -DCMAKE_BUILD_TYPE=Release
+    make -j${MASON_CONCURRENCY}
+    make install
+    mkdir -p ${MASON_PREFIX}/bin
+    mv ../*.jar ${MASON_PREFIX}/bin
+}
+
+function mason_cflags {
+    echo "-I${MASON_PREFIX}/include"
+}
+
+function mason_ldflags {
+    echo "-L${MASON_PREFIX}/lib -ljace"
+}
+
+function mason_clean {
+    make clean
+}
+
+mason_run "$@"
